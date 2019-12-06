ec82b1bdc6847a9a1512f3bfa65a93cea511d8f1
manelmendez <maneeel93@gmail.com>
2019-12-06 03:06:51 +0100

    charting

--- client/src/pages/teams/_id/global.vue
+++ client/src/pages/teams/_id/global.vue
@@ -1,21 +1,14 @@
 <template>
   <v-container fluid>
-      <v-row>
-        <v-col>
-          <statscard :chart-data="datacollection" :height="100"/>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols=4>
-          <statscard :chart-data="datacollection" :height="200"/>
-        </v-col>
-        <v-col cols=4>
-          <statscard :chart-data="datacollection" :height="200"/>
-        </v-col>
-        <v-col cols=4>
-          <statscard :chart-data="datacollection" :height="200"/>
-        </v-col>
-      </v-row>
+    {{team.name}}
+    <v-row>
+      <v-col cols=6>
+        <statscard :chart-data="datacollection" :height="200" :team="team"/>
+      </v-col>
+      <v-col cols=6>
+        <matchstats :chart-data="piedatacollection" :height="200" :team="team"/>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -24,40 +17,81 @@ import { mapActions } from 'vuex'
 import { mapGetters } from 'vuex'
 import constants from '../../../assets/constants/constants'
 import statscard from '../../../components/statscard'
+import matchstats from '../../../components/matchstats'
   export default {
     components: {
-      statscard
+      statscard,
+      matchstats
     },
     data: () => ({
       constants: constants,
     }),
+    async created() {
+      //do something after creating vue instance
+      await this.getTeam(this.$route.params.id)
+      await this.getCompetition(this.team.competition)
+      await this.getCompetitionRounds(this.team.competition)
+    },
     methods: {
       getRandomInt () {
         return Math.floor(Math.random() * (16 - 1)) + 1
       },
-      ...mapActions("team",[
-        'getTeam',
-      ])
+      ...mapActions({
+        getTeam: 'team/getTeam',
+        getCompetition: 'competition/getCompetition',
+        getCompetitionRounds: 'competition/getCompetitionRounds',
+      })
     },
     computed: {
-      ...mapGetters("team",[
-        'team',
-      ]),
-      datacollection: function () {        
+      ...mapGetters({
+        rankedTeams: 'competition/rankedTeams',
+        statsPerRound: 'competition/statsPerRound',
+        team: 'team/team'
+      }),
+      datacollection: function () {      
+        let labels = []
+        let positions = []
+        for (let i = 0; i < this.statsPerRound.length; i++) {
+          // labels.push(this.statsPerRound[i].name)
+          labels.push("J"+(i+1))
+          for (let j = 0; j < this.statsPerRound[i].ranking.length; j++) {
+            if (this.statsPerRound[i].ranking[j].id == this.team.id) {
+              positions.push(j+1)
+            }
+          } 
+        }     
         return {
-          labels: ["J1","J2","J3","J4","J5","J6","J7","J8","J9","J10","J11","J12","J13","J14","J15","J16","J17","J18"],
+          labels: labels,
           datasets: [
             {
               label: 'Posición',
-              data: [this.getRandomInt(), this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(), this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(), this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt(),this.getRandomInt()]
+              data: positions,
+              backgroundColor: "transparent"
+            }, 
+          ],
+        }
+      },
+      piedatacollection() {
+        let victorias = 0
+        let empates = 0
+        let derrotas = 0
+        for (let i = 0; i < this.rankedTeams.length; i++) {
+          if (this.rankedTeams[i].id == this.team.id) {
+            victorias = this.rankedTeams[i].stats.wins
+            empates = this.rankedTeams[i].stats.draws
+            derrotas = this.rankedTeams[i].stats.loses
+          }
+        }        
+        return {
+          labels: ["Victorias","Empates","Derrotas"],
+          datasets: [
+            {
+              data: [victorias, empates, derrotas],
+              backgroundColor: ['rgb(117, 255, 131)','rgb(255, 212, 71)','rgb(255, 117, 117)'],
             }, 
           ]
         }
-      }
-    },
-    created() {
-      //do something after creating vue instance
-      this.getTeam(this.$route.params.id)
+      },
     },
   }
 </script>
@@ -65,6 +99,6 @@ import statscard from '../../../components/statscard'
   .v-sheet--offset {
     top: -24px;
     position: relative;
-    color: '#257747'
+    color: #257747
   }
 </style>
