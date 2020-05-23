e73056196ed1cc7c4caccb0d952f7df8d19c5b20
manelmendez <maneeel93@gmail.com>
2020-05-23 16:38:14 +0200

    eslint con extension

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,16 +1,17 @@
-{
-  "parserOptions": {
+module.exports = {
+  root: true,
+  parserOptions: {
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
-  "extends": ["eslint:recommended", "prettier"],
-  "env": {
+  extends: ["eslint:recommended", "prettier"],
+  env: {
     "browser": true, //enable window. ...
     "es6": true,
     "node": true,
     "jest": true
   },
-  "rules": {
+  rules: {
     "no-console": "warn"
   }
 }
