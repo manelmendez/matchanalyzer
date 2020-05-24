0ed05590a5a3340dee41d225ca8b4422d4ab1eb0
manelmendez <maneeel93@gmail.com>
2020-05-24 14:35:16 +0200

    duckdns

--- client/src/assets/constants/constants.js
+++ client/src/assets/constants/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ADDRESS : process.env.RASPY ? 'https://37.133.11.3:9009/' : 'http://localhost:9000/',
-  API_ADDRESS : process.env.RASPY ? 'https://37.133.11.3:9009/api/' : 'http://localhost:9000/api/',
+  ADDRESS : process.env.RASPY ? 'https://matchanalyzer.duckdns.org/' : 'http://localhost:9000/',
+  API_ADDRESS : process.env.RASPY ? 'https://matchanalyzer.duckdns.org/api/' : 'http://localhost:9000/api/',
   DEFAULT_TEAM_URL: 'assets/images/escudo-vacio.png',
   DEFAULT_PLAYER_URL: 'assets/images/person_icon.png',
   // LOCAL_API_ADDRESS : 'http://37.133.124.200:9000/api/',
