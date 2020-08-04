a5cc2749e5fbce85a8ab265d259d184665d217f5
manelmendez <maneeel93@gmail.com>
2020-08-04 18:00:13 +0200

    bug fix

--- server/resources/matchanalyzer-pg.sql
+++ server/resources/matchanalyzer-pg.sql
@@ -16,9 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE DATABASE matchanalyzer;
-GRANT ALL PRIVILEGES ON DATABASE matchnalyzer TO matchnalyzer;
-
 --
 -- Name: matchanalyzer; Type: SCHEMA; Schema: -; Owner: matchanalyzer
 --
