7e59a563cce65a929065e663c5f53b31c2910332
manelmendez <maneeel93@gmail.com>
2025-02-01 17:49:49 +0100

    fix

--- src/services/image-services.ts
+++ src/services/image-services.ts
@@ -2,18 +2,19 @@ import multer from 'multer'
 import { Request, Response } from 'express'
 
 const storage = multer.diskStorage({
-  destination: function (cb: any) {
-    cb(null, './assets/images')
+  destination: function (req: Request, file: any, cb: any) {
+    cb(null, './src/assets/images')
   },
-  filename: function (file: any, cb: any) {
+  filename: function (req: Request, file: any, cb: any) {
     const filename = `${Date.now()}-${file.originalname}`;
     cb(null, filename);
   }
 })
 
-const imageFilter = (file: any, cb: any) => {
+const imageFilter = (req: Request, file: any, cb: any) => {
+  console.log(file)
   // accept image only
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+  if (!file || !file.originalname || !file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
     return cb(new Error('Only image files are allowed!'), false)
   }
   cb(null, true)
@@ -24,31 +25,31 @@ const upload = multer({ storage, fileFilter: imageFilter }).single(
 )
 
 const uploadImage = async (req: Request, res: Response) => {
-  console.log('Uploading image')
-  console.log(req.body)
-  try {
-    upload(req, res, (err) => {
-      if (err) {
-        console.error(err);
-        if (err.code === 'LIMIT_FILE_SIZE') {
-          return res.status(413).json({ error: 'Image file too large' });
-        } else {
-          return res.status(500).json({ error: 'Failed to upload image' });
-        }
+  upload(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(413).json({ error: 'Image file too large' });
+      } else {
+        return res.status(500).json({ error: 'Failed to upload image' });
       }
-      if (!req.file) {
+    }
+    console.log('Uploading image');
+    console.log(req.file);
+    try {
+      if (!req.file || req.file === undefined) {
         return res.status(400).json({ error: 'No image file uploaded' });
       }
       return res.status(200).json({
         message: 'Image uploaded successfully',
-        filename: req.file.filename,
-        filepath: req.file.path,
+        filename: req.file?.filename,
+        filepath: req.file?.path,
       });
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Internal server error' });
-  }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 }
 
 export default {
